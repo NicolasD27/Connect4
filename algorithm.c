70c791a34cd3482d97c739c0e12b88053c7648f2
piecharton <46318475+piecharton@users.noreply.github.com>
2022-06-11 16:31:25 +0200

    start algorithm

---
+++ algorithm.c
@@ -0,0 +1,16 @@
+#include "connect4.h"
+
+//extern struct map board;
+
+void	compute_turn()
+{
+
+}
+
+int	get_first_empty_tile_height_in_column(int column)
+{
+	int height = 0;
+	while (height < get_height() && board[column][height] != empty)
+		height++;
+	return (height);
+}
